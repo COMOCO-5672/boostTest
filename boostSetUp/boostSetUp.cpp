3f729e2b8b6d96afd9922a6aa3fdcdbc1a898044
jiaopl <jiaopl@bokecc.com>
2023-08-04 10:42:26 +0800

    添加提交内容

--- boostSetUp/boostSetUp.cpp
+++ boostSetUp/boostSetUp.cpp
@@ -8,6 +8,9 @@
 #include "../boostLog/boostLog.h"
 
 #include <iostream>
+#include <atomic>
+
+#include <future>
 
 #pragma comment(lib, "libboost_thread-vc143-mt-gd-x64-1_81.lib")
 #pragma comment(lib, "libboost_coroutine-vc143-mt-gd-x64-1_81.lib")
@@ -42,29 +45,48 @@ void foo(boost::coroutines2::coroutine<void>::pull_type &pull)
     std::cout << "c\n";
 }
 
+int fun(int x, std::promise<int> &p)
+{
+    x++;
+    x *= 10;
+    p.set_value(x);
+    std::cout << "thread_id:" << std::this_thread::get_id() << std::endl;
+    return x;
+}
+
 int main()
 {
-    boost::coroutines2::coroutine<void>::push_type push(foo);
+   /* boost::coroutines2::coroutine<void>::push_type push(foo);
 
     std::cout << "1\n";
     push();
     std::cout << "2\n";
     push();
     std::cout << "3\n";
-    push();
+    push();*/
 
-    std::cout << "main thread" << std::this_thread::get_id() << std::endl;
+    /*std::cout << "main thread" << std::this_thread::get_id() << "\r\n" << std::endl;
     AsyncWrapper wrapper;
     std::string file = "./temp.txt";
     wrapper.async_read(file, [](const std::string &result) {
-        std::cout << "thread_id" << std::this_thread::get_id() << std::endl;
+        std::cout << "thread_id" << std::this_thread::get_id() << "\r\n" << std::endl;
         printf("%s\n", result.c_str());
         });
 
     wrapper.wait();
 
     boost::thread thread(&hello);
-    thread.join();
+    thread.join();*/
+
+    std::promise<int> p;
+    auto fu = p.get_future();
+    std::thread t(fun, 1, std::ref(p));
+    int id = fu.get();
+    std::cout << "get value:" << id << "\r\n" << std::endl;
+    std::cout << "main_thread:" << std::this_thread::get_id() << std::endl;
+
+    std::atomic<int> fo(1);
+    fo.store(10, std::memory_order_seq_cst);
 
     std::system("pause");
 
