89026ee5483415e523aad364466674cafe4e2e32
“jiaopl” <jiaopl@bokecc.com>
2024-03-07 16:39:00 +0800

    添加示例，但是运行失败

--- asio_client/AsioClient/AsioClient.cpp
+++ asio_client/AsioClient/AsioClient.cpp
@@ -2,19 +2,38 @@
 //
 
 #include <iostream>
+#include <thread>
+#include "boost/asio.hpp"
+
+using namespace boost::asio;
+
+void start()
+{
+    io_service service;
+    ip::tcp::socket socket(service);
+
+    int32_t timeout = 1000;
+    setsockopt(socket.native_handle(), SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout));
+    setsockopt(socket.native_handle(), SOL_SOCKET, SO_SNDTIMEO, (const char *)&timeout, sizeof(timeout));
+
+    boost::system::error_code ec;
+    ip::tcp::endpoint ep(ip::address_v4::from_string("::1"), 8001);
+
+    socket_base::keep_alive opt(true);
+    socket.set_option(opt);
+    socket.connect(ep, ec);
+
+    if (ec.failed()) {
+        std::cout << "connect failed" << std::endl;
+    }
+
+    Sleep(-1);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::thread th(start);
+    Sleep(10000);
+    th.join();
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
